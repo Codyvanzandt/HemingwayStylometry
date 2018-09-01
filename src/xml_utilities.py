a72adf1ffa6ba28f993e6a36e969bbb98e51d9a7
Cody VanZandt <cody.a.vanzandt@gmail.com>
2018-09-01 13:09:16 -0500

    initial commit, xml parsing utilities, hemingway data

---
+++ src/xml_utilities.py
@@ -0,0 +1,88 @@
+from xml.etree import ElementTree
+import datetime
+import itertools
+import numpy
+import pandas
+
+DEFAULT_NAMESPACE = NS = "{http://www.tei-c.org/ns/1.0}"
+
+#########################
+# XML Parsing Functions #
+#########################
+
+
+def makeBorrowingDataFrame(fileName):
+    document = parseXMLDocument(fileName)
+    borrowingEventNodes = getAllBorrowingEventNodes(document)
+    borrowedEventData = [getBorrowingEventData(
+        node) for node in borrowingEventNodes]
+    return pandas.DataFrame(borrowedEventData)
+
+
+def parseXMLDocument(filename):
+    return ElementTree.parse(filename)
+
+
+def getAllBorrowingEventNodes(document):
+    return document.findall(f".//{NS}ab[{NS}bibl]")
+
+
+def getBorrowingEventData(borrowingEvent):
+    return {"Title": getTitle(borrowingEvent),
+    "Date Borrowed": getDateBorrowed(borrowingEvent),
+    "Date Returned": getDateReturned(borrowingEvent)
+    }
+
+
+def getTitle(borrowingEvent):
+    titleNode = getTitleNode(borrowingEvent)
+    if titleNode is None:
+        return str()
+    else:
+        return titleNode.text
+
+def getTitleNode(borrowingEvent):
+    return borrowingEvent.find(f".{NS}bibl[@ana='#borrowedItem']/{NS}title")
+
+def getDateBorrowed(borrowingEvent):
+    dateBorrowedNode = getDateBorrowedNode(borrowingEvent)
+    return parseDateNodeIntoDate(dateBorrowedNode)
+
+def getDateBorrowedNode(borrowingEvent):
+    return borrowingEvent.find(f".{NS}date[@ana='#checkedOut']")
+
+def getDateReturned(borrowingEvent):
+    dateReturnedNode = getDataReturnedNode(borrowingEvent)
+    return parseDateNodeIntoDate(dateReturnedNode)
+
+def getDataReturnedNode(borrowingEvent):
+    return borrowingEvent.find(f".{NS}date[@ana='#returned']")
+
+
+def parseDateNodeIntoDate(dateNode):
+    if dateNode is None:
+        return numpy.datetime64("NaT")
+    else:
+        date = dateNode.get("when")
+        if date is None:
+            return numpy.datetime64("NaT")
+        else:
+            return stringToDate(date)
+
+def stringToDate(dateString):
+    try:
+        return datetime.datetime.strptime(dateString, "%Y-%m-%d").date()
+    except ValueError:
+        try:
+            return datetime.datetime.strptime(dateString, "%Y-%m").date()
+        except ValueError:
+            try:
+                return datetime.datetime.strptime(dateString, "%Y").date()
+            except ValueError:
+                return numpy.datetime64("NaT")
+
+pandas.set_option('display.max_columns', None)
+pandas.set_option('display.max_rows', None)
+
+hemFile="hemingway.tei.xml"
+print(makeBorrowingDataFrame(hemFile))
